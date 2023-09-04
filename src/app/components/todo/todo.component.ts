109287b7509e8b6a995fbbc004da1d4917603a77
Ijaas Fouzer <96341377+IjaasMohamed@users.noreply.github.com>
2023-09-04 10:50:10 +0530

    content-display

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit{
+
+  todos !: Todo[];
+  constructor(){}
+
+  ngOnInit(): void {
+    this.todos = [
+      {
+      content:'First content',
+      completed:true
+      },
+      {
+        content:'Second content',
+        completed:true
+      }
+    ]
+  }
 
 }
